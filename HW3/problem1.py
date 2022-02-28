cb138d0017063d96dadde5970d12e2875833d0b9
vardanarevshatyan <vardan.arevshatyan.job@gmail.com>
2022-02-28 14:11:59 +0400

    HW3 p1

--- HW3/problem1.py
+++ HW3/problem1.py
@@ -1,11 +1,21 @@
-class SpanishSpeaker:
+class EnglishSpeaker:
+    """Something in English. This is the target class."""
     @staticmethod
     def order(text) -> str:
+        print('English speaker said: ', text)
+        return text
+
+
+class SpanishSpeaker:
+    """Something in Spanish. This is the Adaptee."""
+    @staticmethod
+    def spanish_order(text) -> str:
         print('Spanish speaker said: ', text)
         return text
 
 
 class EnglishBarman:
+    """This is the client class working with the target. """
     request_list = {'A cup of coffee, please.': 'Your coffee will be ready in 5 minutes, thank you for the order.',
                     'Do you have sushi?': 'We do not serve sushi.',
                     'Can I speak to your manager?': 'Our manager is out of office right now.'}
@@ -13,21 +23,37 @@ class EnglishBarman:
     @classmethod
     def get_the_order(cls, text):
         en_text = cls.request_list.get(text, 'Sorry, I did not quite understand.')
-        print('Waiter replied: ', en_text)
+        print('Barman replied: ', en_text)
         return text
 
 
+class TranslatorFriend(SpanishSpeaker, EnglishSpeaker):
+    translations = {'Una taza de café por favor.': 'A cup of coffee, please.',
+                    '¿Tienes sushi?': 'Do you have sushi?',
+                    '¿Puedo hablar con su gerente?': 'Can I speak to your manager?'}
 
-class SpanishWaiter(SpanishSpeaker, EnglishBarman):
+    """Translator speaks Spanish and acts as the Adapter."""
     @classmethod
-    def get_the_order(cls, text):
-        en_text = cls.request_list.get(cls.order(text), 'Sorry, I did not quite understand.')
-        print('Waiter replied: ', en_text)
+    def order(cls, text):
+        en_text = cls.translations.get(text, 'I cannot translate that.')
+        print('Translator translated to barman: ', en_text)
         return en_text
 
 
 if __name__ == '__main__':
+    english_client = EnglishSpeaker()
     spanish_client = SpanishSpeaker()
-    waiter = EnglishBarman()
-    spanish_sentence = spanish_client.order('A cup of coffee, please.')
-    waiter.get_the_order(spanish_sentence)
+    barman = EnglishBarman()
+    translator = TranslatorFriend()
+    print(10*'*', 'Client communicates with the target.', 10*'*')
+    english_order = english_client.order('A cup of coffee, please.')
+    barman.get_the_order(english_order)
+    print(50*'*', '\n')
+    print(10*'*', 'Client and Adaptee cannot communicate.', 10*'*')
+    sp_order = spanish_client.spanish_order('¿Tienes sushi?')
+    barman.get_the_order(sp_order)
+    print(50 * '*', '\n')
+    print(10*'*', 'Adapter saves the situation.', 10*'*')
+    translated_order = translator.order(sp_order)
+    barman.get_the_order(translated_order)
+    print(50 * '*', '\n')
